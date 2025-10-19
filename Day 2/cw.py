93ece41d9f11d4e21b8fdac947414c7c20571f4d
davi-turi <ninodavituri45@gmail.com>
2025-10-19 23:19:48 +0400

    fourth commit

---
+++ Day 2/cw.py
@@ -0,0 +1,30 @@
+1.
+#ცვლადი არის ინფორმაციის შემნახველი ყუთი,სადაც ინახება ნებისმიერი სახის ინფორმაცია.
+
+2.
+food = "khinkali"
+
+3.
+print(food)
+print("goal oriented academy")
+print(50)
+print(8.9)
+
+4.
+movie = "the fast and the furious"
+movie = "ten things I hate about you"
+print(movie)
+
+5.
+print("nini" + "davituri")
+
+6.
+# კოდი print("house * 3") ტერმინალში გამოიტანს სტრინგ house-ს სამჯერ.
+
+7.
+print("this is string")
+surname = "davituri"
+car = "mercedes"
+
+8.
+# რადგან, lesson  არის ცვლადი და არ არის მოცემული ბრჭყალებში.ამ დროს კომპიუტერს ვთხოვთ ისეთი რაღაცის დაპრინტვას, რაც არ არსებობს.
